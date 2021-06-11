6c408621c9546a6de639768fd3adf4f8625a2105
MohamedAteya <mohamedateya883@gmail.com>
2021-06-11 06:13:43 +0200

    changes to modules

--- stream_c.py
+++ stream_c.py
@@ -1,6 +1,30 @@
 import torch
 import torch.nn as nn
-from layers.efficient_x3d_xs import E_x3d_xs
-from layers.concate import Concate
+from layers import efficient_x3d_xs, r2plus1d_18
 
+
+class Stream_A(nn.Module):
+    """
+    Args:
+        model: str, a strig with the right name of the needed model, now supported "x3d", "r2plus1d" 
+    """
+    def __init__(self, model, out_dim, trainable = True):
+        super(Stream_A, self).__init__()
+        
+        assert model in ['x3d', 'r2plus1d'] , "models suported for stream A are 'x3d', 'r2plus1d'"
+
+        if model == 'x3d':
+            self.model = efficient_x3d_xs.E_x3d_xs(out_dim)
+        elif model == 'r2plus1d':
+            self.model = r2plus1d_18.R2plus1d(out_dim)
+
+        if not trainable:
+            for i, param in enumerate(self.model.parameters()):
+                param.requires_grad = False
+            self.model.fc.requires_grad = True
+    
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
 
