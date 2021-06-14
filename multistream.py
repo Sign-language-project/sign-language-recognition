a6179c5639cf6e8b0ce27f7296b41bf0016d4de3
mohieEldeen <65570007+mohieEldeen@users.noreply.github.com>
2021-06-14 19:19:37 +0200

    stream b only

--- multistream.py
+++ multistream.py
@@ -10,12 +10,12 @@ class Stream_bc (nn.Module):
 
     def __init__(self ,models , trainables, concat_dim, num_classes, device, raft_parameters_path, raft_iters = 12):
       """
-      a model for training the streams b and c together. 
+      a model for training the streams b and c together.
 
       args:
       ==================================
         models (list[str]) : a list that contain the chosen model for every stream. (models[0] >> stream b , models[1] >> stream c)
-        trainables (list[bool]) : to choose whether to train the pretrained models of b and c or not. 
+        trainables (list[bool]) : to choose whether to train the pretrained models of b and c or not.
         concat_dim : the output dim of the pretrained models. the dim at which the output of the streams will be concated together.
         num_classes : the number of classes of the main dataset.
         device : the device at which to train the model.
@@ -24,7 +24,7 @@ class Stream_bc (nn.Module):
       """
 
       super().__init__()
-      
+
       self.stream_b_model = Stream_b(models[0], concat_dim, raft_parameters_path, device, raft_iters , trainables[0])
 
       self.stream_c_model = Stream_c(models[1], concat_dim, trainables[1])
@@ -45,6 +45,47 @@ class Stream_bc (nn.Module):
       stream_c_out = self.stream_c_model(videos) #(b, concat_dim)
 
       out = self.concat([stream_b_out, stream_c_out]) #(b  , concat_dim)
+
+      out = self.linear(out) #(b  , num_classes)
+
+      return out
+
+
+class Stream_b_only (nn.Module):
+
+    def __init__(self , model, concat_dim, num_classes, raft_parameters_path, device, raft_iters = 12 , trainable = True):
+      """
+      a model for training the streams only.
+
+      args:
+      ==================================
+        model (str) :  the chosen model to train the stream b on.
+        trainable (bool) : to choose whether to train the pretrained model of bor not.
+        concat_dim : the output dim of the pretrained model. the dim at which the output of the streams will be concated together.
+        num_classes : the number of classes of the main dataset.
+        device : the device at which to train the model.
+        raft_parameters_path : the path of the pretrained raft parameters.
+        raft_iters : number of iterations of the raft.
+      """
+
+      super().__init__()
+
+      self.stream_b_model = Stream_b(model, concat_dim, raft_parameters_path, device, raft_iters , trainable)
+
+      self.concat = Concate(n_streams = 1, in_dim = concat_dim, out_dim = concat_dim)
+
+      self.linear = nn.Linear(concat_dim, num_classes)
+
+
+    def forward(self, x):
+      #X >> tuple of two (videos , videos_raft)
+
+      videos = x[0]         #(B, C, T, H, W)
+      videos_raft = x [1]   #(B, C, T, H, W)
+
+      stream_b_out = self.stream_b_model(videos_raft) #(b, concat_dim)
+
+      out = self.concat([stream_b_out]) #(b  , concat_dim)
 
       out = self.linear(out) #(b  , num_classes)
 
