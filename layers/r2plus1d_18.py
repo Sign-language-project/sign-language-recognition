191d0e5ed5d5d5839704e8c5598b972f2a9aa9d6
mohieEldeen <65570007+mohieEldeen@users.noreply.github.com>
2021-06-15 08:33:58 +0200

    r2plus1 edits

--- layers/r2plus1d_18.py
+++ layers/r2plus1d_18.py
@@ -8,11 +8,11 @@ class R2plus1d(nn.Module):
     super(R2plus1d, self).__init__()
     self.model = torchvision.models.video.r2plus1d_18(pretrained= True)
     self.relu = nn.ReLU()
-    self.fc = nn.Linear(500, out_dim)
+    self.fc = nn.Linear(400, out_dim)
 
 
   def forward(self, x):
-    x = self.model(x) 
+    x = self.model(x)
     x = self.relu(x)
-    x = self.fc(X)
+    x = self.fc(x)
     return x
